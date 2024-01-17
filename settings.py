affa028b6e8dc15ff6d245c0e80424f59dd3ae0e
Dew2003 <152517848+Dew2003@users.noreply.github.com>
2024-01-17 08:51:10 +0000

    change

--- settings.py
+++ settings.py
@@ -19,9 +19,9 @@ IMAGE = 'Image'
 VIDEO = 'Video'
 WEBCAM = 'WebCam'
 #RTSP = 'RTSP'
-#YOUTUBE = 'YouTube'
+YOUTUBE = 'YouTube'
 
-SOURCES_LIST = [IMAGE, VIDEO, WEBCAM]
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM,YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
@@ -29,7 +29,7 @@ DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
 DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 # Videos config
-VIDEO_DIR = ROOT 
+VIDEO_DIR = ROOT/'videos' 
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
 VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
 VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
